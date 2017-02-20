40ba52135a11dcd0964804d4cd744f04ab762e41
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-02-20 13:35:51 +0700

    001

--- app/src/main/java/com/example/teo/a27json001/MainActivity.java
+++ app/src/main/java/com/example/teo/a27json001/MainActivity.java
@@ -1,17 +1,47 @@
 package com.example.teo.a27json001;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
     private ListView lv;
 
+    ArrayList<HashMap<String, String>> contactList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contactList = new ArrayList<>();
+        lv = (ListView)findViewById(R.id.list);
+
+        //new GetContacts().execute();
+    }
+
+
+    private class GetContacts extends AsyncTask<Void, Void, Void>{
+
+        public void execute() {
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this, "Json Data is downloading", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
     }
 }
